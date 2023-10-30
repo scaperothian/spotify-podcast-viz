de841f4bf547db259ee1043056133d57e3dd4995
Jayant Tiwari <tijayant@amazon.com>
2023-10-29 17:06:20 -0700

    working publisher query box

--- w209.py
+++ w209.py
@@ -1,58 +1,18 @@
-# from flask import Flask, render_template
-# app = Flask(__name__)
-
-# @app.route("/")
-# def w209():
-#     file="about9.jpg"
-#     return render_template("w209.html",file=file)
-
-# if __name__ == "__main__":
-#     app.run()
-
-
-from flask import Flask, render_template
-# from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request, jsonify
 import altair as alt
 import pandas as pd
+import random
+import json
 
 app = Flask(__name__)
-# Bootstrap(app)
-# @app.route('/')
-# def index():
-#     # Sample data
-#     data = pd.DataFrame({
-#         'a': list('CCCDDDEEE'),
-#         'b': [2, 7, 4, 5, 3, 2, 7, 2, 4]
-#     })
 
-#     # Altair chart
-#     chart = alt.Chart(data).mark_bar().encode(
-#         x='a',
-#         y='b'
-#     )
-#     print(chart)
-#     # Convert chart to JSON
+with open("visualization_data/publisher/publisher.json") as fp:
+    publisher_data = json.load(fp)
 
-#     chart_json = chart.to_dict()
-#     print(chart_json)
-#     return render_template('index.html', chart=chart_json)
-
-@app.route('/')
-def index():
-    return render_template('base.html')
-
-
-@app.route('/publisher')
-def publisher():
-    # data = load_data('publisher')  # Load publisher data
-    # chart = alt.Chart(data).mark_bar().encode(
-    #     x='name:N',
-    #     y='count:Q'
-    # )
-
+def get_chart():
     data = pd.DataFrame({
         'a': list('CCCDDDEEE'),
-        'b': [2, 7, 4, 5, 3, 2, 7, 2, 4]
+        'b': [random.randint(1, 100) for _ in range(9)]
     })
 
     # Altair chart
@@ -60,29 +20,36 @@ def publisher():
         x='a',
         y='b'
     )
-    print(chart)
-    # Convert chart to JSON
+    return chart
+    
 
-    chart_dict = chart.to_dict()
-    return render_template('publisher.html', chart=chart_dict)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/publisher')
+def publisher():
+    return render_template('publisher.html', publishers=list(publisher_data.keys()))
 
 @app.route('/shows')
 def shows():
-    # data = load_data('publisher')  # Load publisher data
-    # chart = alt.Chart(data).mark_bar().encode(
-    #     x='name:N',
-    #     y='count:Q'
-    # )
     return render_template('shows.html', chart={})
 
 @app.route('/episodes')
 def episodes():
-    # data = load_data('publisher')  # Load publisher data
-    # chart = alt.Chart(data).mark_bar().encode(
-    #     x='name:N',
-    #     y='count:Q'
-    # )
     return render_template('episodes.html', chart={})
+
+@app.route('/general')
+def general():
+    return render_template('general_insights.html')
+
+@app.route('/get-publisher-visualization', methods=['POST'])
+def get_publisher_visualization():
+    publisher_name = request.json.get('publisher_name')
+    selected_publisher_data = publisher_data[publisher_name]
+
+    return jsonify(selected_publisher_data)
 
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True

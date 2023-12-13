3d5cfc4f0570582e40bf1626b54105037e6318fe
Jayant Tiwari <tijayant@berkeley.edu>
2023-12-13 14:26:54 -0800

    working search with chart

--- w209.py
+++ w209.py
@@ -8,11 +8,11 @@ from elasticsearch import Elasticsearch
 from altair import Chart, X, Y, Data
 client = Elasticsearch(
   "https://527db13b131f4f1b90590b6f6c0039f6.us-central1.gcp.cloud.es.io:443",
-  api_key=""
+  api_key="STFJVlc0d0JRcERJVVMzblRzTG86OHBDcEUzLTNUeHVENUdISmxmRlZ2dw=="
 )
 
 #convert elsatic search to dataframe
-docs = client.search(index='search-spotidy-dataset', body={"query": {"match_all": {}}})
+docs = client.search(index='search-spotify-dataset-visualizer', body={"query": {"match_all": {}}})
 spotifyresults = [hit['_source']['doc'] for hit in docs['hits']['hits']]
 spotify_df = pd.json_normalize(spotifyresults)
 
@@ -95,12 +95,75 @@ def get_episode_visualization():
 def get_spotify_data_from_elasticsearch():
 
     query_phrase = request.json.get("query_phrase")
-    res = client.search(index="search-spotidy-dataset", q=query_phrase)
-    print(res)
+    es_query = {
+        "size": 200,
+        "query": {
+            "bool": {
+            "must": {
+                "multi_match": {
+                "query": query_phrase,
+                "fields": ["doc.show_name", "doc.show_description", "doc.episode_name", "doc.episode_description", "doc.publisher"],
+                "type": "best_fields",
+                "operator": "or"
+                }
+            }
+            }
+        }
+    }
+    filters = request.json.get("filters")
+
+    res = client.search(index='search-spotify-dataset-visualizer', body=es_query)
+
+
+    print(filters)
+    show_filter = []
+    publisher_filter = []
+
+    for k,v in filters['show_filters'].items():
+        if v:
+            show_filter.append(k)
+
+    for k,v in filters['publisher_filters'].items():
+        if v:
+            publisher_filter.append(k)
+
     results = [hit['_source']['doc'] for hit in res['hits']['hits']]
     # at some point we need to turn these results into a dataframe to make the visualizations
     ## build filters and types
-    return jsonify(results)
+
+    results1, results2 = [], []
+    # apply filters
+    if publisher_filter:
+        results1 = list(filter(lambda x: x['publisher'] in publisher_filter, results))
+
+    # apply filters
+    if show_filter:
+        results2 = list(filter(lambda x: x['show_name'] in show_filter, results))
+
+
+    if not publisher_filter and not show_filter:
+
+        avg_duration = sum([x['duration'] for x in results]) / len(results)
+        return jsonify(
+            {
+                'rows':results[:50],
+                'average_duration': round(avg_duration)
+            }
+        )
+    else:
+
+        new_result = results1 + results2
+        avg_duration = sum([x['duration'] for x in new_result]) / len(new_result)
+
+        return jsonify({
+            'rows': results1 + results2,
+            'average_duration': round(avg_duration)
+        })
+    
+
+
+    
+
 
 #put altair code to generate charts here
 @app.route("/gen")
@@ -113,6 +176,6 @@ def gen():
     return chart.to_json()
 
 if __name__ == '__main__':
-    app.jinja_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    # app.jinja_env.auto_reload = True
+    # app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True)
